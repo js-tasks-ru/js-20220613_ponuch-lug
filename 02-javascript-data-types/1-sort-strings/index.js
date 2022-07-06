b6b07bed2325c3f2229b1f7f3561bc043063258a
ponuch <ponuch.lug@gmail.com>
2022-07-06 17:04:36 +0300

    sortString added

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    const locales = ['ru', 'en'];
+    if (param === 'asc') {
+        return arr.slice().sort((a, b) => a.localeCompare(b, locales, {caseFirst: 'upper'}))
+    }
+    else if (param === 'desc') {
+        return arr.slice().sort((a, b) => b.localeCompare(a, locales, {caseFirst: 'upper'}))
+    }
 }
+
+//console.log(sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'], 'desc'))
